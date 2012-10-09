508b287a586f3ab8ed1115225fc21d69d13ffd03
Nicolas Brassard <nico@nitriques.com>
2012-10-09 17:04:10 -0400

    Added French translation

---
+++ lang/lang.fr.php
@@ -0,0 +1,21 @@
+<?php
+
+	$about = array(
+		'name' => 'Français',
+		'author' => array(
+			'name' => 'Deux Huit Huit',
+			'email' => 'open-source@deuxhuithuit.com',
+			'website' => 'http://www.deuxhuithuit.com'
+		),
+		'release-date' => '2012-10-09'
+	);
+
+	/**
+	 * Unpublished Filter
+	 */
+	$dictionary = array(
+
+		'published' => 
+		'publié'
+
+	);
