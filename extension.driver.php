a1d3071202e01458d72fc17553c43506dd942f39
Nils Hörrmann <post@nilshoerrmann.de>
2015-09-11 10:23:43 +0200

    Code formatting

--- extension.driver.php
+++ extension.driver.php
@@ -2,63 +2,62 @@
 
 class Extension_Unpublishedfilter extends Extension
 {
+    /**
+     * Add callback functions to backend delegates
+     */
+    public function getSubscribedDelegates()
+    {
+        return array(
+            array(
+                'page' => '/backend/',
+                'delegate' => 'InitaliseAdminPageHead',
+                'callback' => 'initaliseAdminPageHead'
+            )
+        );
+    }
 
-	/**
-	 * Add callback functions to backend delegates
-	 */
-	public function getSubscribedDelegates()
-	{
-		return array(
-			array(
-				'page' => '/backend/',
-				'delegate' => 'InitaliseAdminPageHead',
-				'callback' => 'initaliseAdminPageHead'
-			)
-		);
-	}
+    /**
+     * Add JavaScript
+     */
+    public function initaliseAdminPageHead($context)
+    {
+        $callback = Symphony::Engine()->getPageCallback();
 
-	/**
-	 * Add JavaScript
-	 */
-	public function initaliseAdminPageHead($context)
-	{
-		$callback = Symphony::Engine()->getPageCallback();
+        // Append javascript for publish table
+        if ($callback['driver'] == 'publish' && $callback['context']['page'] == 'index') {
+            $settings  = Symphony::Configuration()->get('unpublishedfilter');
 
-		// Append javascript for publish table
-		if($callback['driver'] == 'publish' && $callback['context']['page'] == 'index') {
-			$settings  = Symphony::Configuration()->get('unpublishedfilter');
+            Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/unpublishedfilter/assets/unpublishedfilter.publish.css', 'screen', 2000);
+            Administration::instance()->Page->addScriptToHead(URL . '/extensions/unpublishedfilter/assets/unpublishedfilter.publish.js', 2002);
 
-			Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/unpublishedfilter/assets/unpublishedfilter.publish.css', 'screen', 2000);
-			Administration::instance()->Page->addScriptToHead(URL . '/extensions/unpublishedfilter/assets/unpublishedfilter.publish.js', 2002);
+            if ($settings['filter-by-dates'] === false) {
+                Administration::instance()->Page->addElementToHead(
+                    new XMLElement(
+                        'script',
+                        'Symphony.Context.add(\'filter-by-dates\', false);',
+                        array(
+                            'type' => 'text/javascript'
+                        )
+                    )
+                );
+            }
+        }
+    }
 
-			if ($settings['filter-by-dates'] === false) {
-				Administration::instance()->Page->addElementToHead(
-					new XMLElement(
-						'script',
-						'Symphony.Context.add(\'filter-by-dates\', false);',
-						array(
-							'type' => 'text/javascript'
-						)
-					)
-				);
-			}
-		}
-	}
+    public function install()
+    {
+        Symphony::Configuration()->set('filter-by-dates', true, 'unpublishedfilter');
+        Symphony::Configuration()->write();
+    }
 
-	public function install()
-	{
-		Symphony::Configuration()->set('filter-by-dates', true, 'unpublishedfilter');
-		Symphony::Configuration()->write();
-	}
+    public function update($previousVersion)
+    {
+        $this->install();
+    }
 
-	public function update($previousVersion)
-	{
-		$this->install();
-	}
-
-	public function uninstall()
-	{
-		Symphony::Configuration()->remove('unpublishedfilter');
-		Symphony::Configuration()->write();
-	}
+    public function uninstall()
+    {
+        Symphony::Configuration()->remove('unpublishedfilter');
+        Symphony::Configuration()->write();
+    }
 }
